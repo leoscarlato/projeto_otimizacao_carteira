fb5c5e7e4c9f198346c31ff9dc6d6644b5888edc
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2025-05-05 12:53:48 -0300

    fetch data

---
+++ data_loader/data_loader.py
@@ -0,0 +1,11 @@
+import yfinance
+
+acoes = [
+    "UNH", "GS", "MSFT", "HD", "V", "SHW", "MCD", "CAT", "AMGN", "AXP",
+    "TRV", "CRM", "IBM", "JPM", "AAPL", "HON", "AMZN", "PG", "BA", "JNJ",
+    "CVX", "MMM", "NVDA", "WMT", "DIS", "MRK", "KO", "CSCO", "NKE", "VZ"
+]
+
+data = yfinance.download(acoes, start="2024-08-01", end="2024-12-31")
+data_final = data['Close']
+data_final.to_csv('data_loader/acoes.csv')

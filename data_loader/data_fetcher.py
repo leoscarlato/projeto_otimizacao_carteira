d8a0fab50304efee0b5830123816e115e560d9b9
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2025-05-09 19:38:41 -0300

    refactor: excluindo funções desnecessárias e melhorando o fetcher

--- data_loader/data_fetcher.py
+++ data_loader/data_fetcher.py
@@ -1,9 +1,4 @@
-import os
-import certifi
 import yfinance
-
-os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
-os.environ['CURL_CA_BUNDLE'] = certifi.where()
 
 acoes = [
     "UNH", "GS", "MSFT", "HD", "V", "SHW", "MCD", "CAT", "AMGN", "AXP",
@@ -13,11 +8,8 @@ acoes = [
 
 try:
     data = yfinance.download(acoes, start="2024-08-01", end="2024-12-31")
-    if data.empty:
-        print("❌ Falha ao baixar dados: DataFrame vazio!")
-    else:
-        data_final = data['Close']
-        data_final.to_csv('data_loader/acoes.csv')
-        print("✅ Dados salvos com sucesso em 'data_loader/acoes.csv'.")
+    data_final = data['Close']
+    data_final.to_csv('data_loader/acoes.csv')
+    print("Dados salvos com sucesso em 'data_loader/acoes.csv'.")
 except Exception as e:
-    print(f"❌ Erro ao baixar dados: {e}")
+    print(f"Erro ao baixar dados: {e}")
